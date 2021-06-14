4e07fa72ff51d985ecefb4e1868bf67e690d0f7c
zemult <75642470+zemult@users.noreply.github.com>
2021-06-14 10:07:17 -0400

    awdadw

---
+++ index.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  console.log("No crash");
+};
+
+const btn = document.getElementById('inv')
+
+btn.addEventListener('click', () => {
+    alert('Surreal is currently still in development! Check back soon!')
+})
